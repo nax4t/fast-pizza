48af25a5fa100774c721e23c5f4fd9738de0a399
Mohsin Pannu <mohsin.pannu170@gamil.com>
2024-11-17 07:57:33 +0500

    implemented my first data loader in router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Home from './ui/Home';
-import Menu from './features/menu/Menu';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder'
 import Order from './features/order/Order'
@@ -18,7 +18,8 @@ const router = createBrowserRouter([
       },
       {
         path: '/menu',
-        element: <Menu />
+        element: <Menu />,
+        loader: menuLoader
       },
       {
         path: '/cart',
